e758c499a8a72b94fe75ba77d27aaddb5b921152
Sami Sherzaman <153662652+samikhan1622@users.noreply.github.com>
2024-03-21 04:27:55 +0500

    Add files via upload
    Excited to unveil my latest project on GitHub - the Countdown Timer! ⏳💻
    
    Project Overview:
    In today's fast-paced world, effective time management is crucial. With the Countdown Timer project, I aimed to create a versatile tool that helps users track and manage their time efficiently. Whether it's for personal productivity, event planning, or project deadlines, this countdown timer empowers users to stay organized and focused on their goals.

---
+++ index.ts
@@ -0,0 +1,44 @@
+import { differenceInSeconds, isDate } from 'date-fns';
+import inquirer from 'inquirer';
+
+
+const res = await inquirer.prompt({
+    type:"number",
+    name:"userinput",
+    message:"please enter the seconds you want to run.",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "please enter valid number."
+        } else if (input > 60) {
+            return "seconds must in 60"
+        } else {
+            return true;
+        };
+    },
+});
+
+let input = res.userinput
+
+function timeStarter (val: number) {
+    const intTime = new Date().setSeconds (new Date().getSeconds() + val)
+    const intervalTime = new Date (intTime)
+    setInterval(()=>{
+        const currentTime = new Date();
+        const timeDiff = differenceInSeconds(intervalTime, currentTime);
+            if (timeDiff <= 0) {
+                console.log("TIme has expired.");
+                process.exit();   
+        };
+    
+        const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
+        const sec = Math.floor(timeDiff % 60);
+        console.log(`${min.toString().padStart(2, "0")}, ${sec.toString().padStart(2, "0")}`);
+        
+    },1000);
+};
+
+timeStarter(input);
+   
+
+    
+
